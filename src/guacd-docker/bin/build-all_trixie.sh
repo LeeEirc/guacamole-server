c9519441c3241117114d9b1892a8f04b71e68a66
Eric <xplzv@126.com>
2025-11-11 17:55:23 +0800

    perf: remove openssl build

--- src/guacd-docker/bin/build-all_trixie.sh
+++ src/guacd-docker/bin/build-all_trixie.sh
@@ -126,7 +126,7 @@ esac
 # Build and install core protocol library dependencies
 #
 
-install_from_git "https://github.com/openssl/openssl" "$WITH_OPENSSL" $OPENSSL_OPTS
+# install_from_git "https://github.com/openssl/openssl" "$WITH_OPENSSL" $OPENSSL_OPTS
 install_from_git "https://github.com/FreeRDP/FreeRDP" "$WITH_FREERDP" $FREERDP_OPTS $FREERDP_OPTS_OVERRIDES
 install_from_git "https://github.com/libssh2/libssh2" "$WITH_LIBSSH2" $LIBSSH2_OPTS
 install_from_git "https://github.com/seanmiddleditch/libtelnet" "$WITH_LIBTELNET" $LIBTELNET_OPTS
